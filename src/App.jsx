8b81a0ca4d132830f0cc8873362adb2dc3809bb7
Muhammad Haram <haramh643@gmail.com>
2024-05-17 12:43:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <>
+      <div className='bg-slate-600'>App</div>
+      <Button>click kr</Button>
+    </>
+  )
+}
+
+export default App
