ea02dbe465fb9d63fc9cb40745add4617f0ab07d
Muhammad Haram <haramh643@gmail.com>
2024-05-25 03:43:19 +0500

    add side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Button } from './components/ui/button'
 import Header from './components/Header'
+import SideNav from './components/SideNav'
 
 const App = () => {
   return (
     <>
-      <Header/>
+      <Header />
+      <div className='w-64 fixed'>
+        <SideNav />
+      </div>
+      <div className='ml-64'>
+        body
+      </div>
     </>
   )
 }
---
+++ src/components/SideNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideNav = () => {
+  return (
+    <div>
+      sideBar
+    </div>
+  )
+}
+
+export default SideNav
