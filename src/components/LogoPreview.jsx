d3c123cc197efa0921d1814c4513ae816836a865
Muhammad Haram <haramh643@gmail.com>
2024-05-27 20:03:49 +0500

    commit

--- src/components/LogoPreview.jsx
+++ src/components/LogoPreview.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const LogoPreview = () => {
     return (
-       <div className='flex items-center justify-center h-screen'>
-         <div className='w-[500px] h-[500px] bg-gray-200 outline-dotted outline-gray-300'>
-            m
+        <div className='flex items-center justify-center h-screen'>
+            <div className='w-[500px] h-[500px] bg-gray-200 outline-dotted outline-gray-300'>
+                m
+            </div>
         </div>
-       </div>
     )
 }
 
