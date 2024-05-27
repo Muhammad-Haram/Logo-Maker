375e321a3afbd4116a86ae844c712feeb369572c
Muhammad Haram <haramh643@gmail.com>
2024-05-28 04:45:13 +0500

    add some funcionality and use context hook

--- src/components/LogoPreview.jsx
+++ src/components/LogoPreview.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const LogoPreview = () => {
+
+
+    const [storageValue, setStorageValue] = useState();
+
+    useEffect(() => {
+    const storageData = JSON.parse(localStorage.getItem('value'));
+    setStorageValue(storageData)
+    }, [])
+    
+
     return (
         <div className='flex items-center justify-center h-screen'>
             <div className='w-[500px] h-[500px] bg-gray-200 outline-dotted outline-gray-300'>
-                m
+                <div className='h-full w-full' style={{
+                    borderRadius: storageValue?.bgrounded,
+                    backgroundColor :storageValue?.bgcolor
+                }}>
+
+                </div>
             </div>
         </div>
     )
